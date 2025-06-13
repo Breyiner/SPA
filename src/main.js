0a243e1092fa5ce0a0ea42ad1a0c7d5070b3ef6d
Breyiner <breyneracosta7@gmail.com>
2025-06-13 11:04:22 -0500

    clase de usar hashchange

--- src/main.js
+++ src/main.js
@@ -1,2 +1,47 @@
 import './style.css'
 
+let contenedor = document.querySelector('.contenedor');
+
+let rutas = [
+  {
+    name: 'Categorias',
+    path: './src/views/Categorias/index.html',
+    controller : './src/views/Categorias/categoriasController.js'
+  },
+  {
+    name: 'Productos',
+    path: './src/views/Productos/index.html',
+    controller : './src/views/Productos/productosController.js'
+  }
+]
+
+let solicitud = async (ruta) => {
+  let data = await fetch(ruta);
+
+  return data.text();
+}
+
+let obtenerInformacion = (vista) => {
+
+  return rutas.find(elemento => elemento.name === vista);
+}
+
+let cargarVista = async (vista) => {
+
+  let informacion = obtenerInformacion(vista);
+  
+  let contenido = await solicitud(informacion.path);
+  contenedor.innerHTML = contenido;
+}
+
+window.addEventListener("hashchange", () => {
+  let vista = location.hash.slice(1);
+
+  cargarVista(vista);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  let vista = location.hash.slice(1);
+
+  cargarVista(vista);
+})
