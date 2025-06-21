e7a77410f112f3ca74291f88a39a5ad15f93f985
Breyiner <breyneracosta7@gmail.com>
2025-06-20 21:12:15 -0500

    se agregan validaciones al formulario de agregar categoria

--- src/helpers/validaciones.js
+++ src/helpers/validaciones.js
@@ -1,11 +1,58 @@
-const teclasEspeciales = ["Backspace", "Tab", "Enter", "ArrowLeft", "ArrowRight", "Delete"]; // Teclas especiales que se permiten
+const teclasEspeciales = ["Backspace", "Tab", "Enter", "ArrowLeft", "ArrowRight", "Delete"];
 
 export const validarTexto = (event) => {
-  const key = event.key; // Obtenemos la tecla presionada
-  const regex = /^[\D]*$/i;  // Expresión regular para letras y caracteres especiales
+  const key = event.key;
+  const regex = /^[\D]*$/i;
 
-  if (!regex.test(key) && !teclasEspeciales.includes(key)) { // Validamos si la tecla no es una letra o si el campo supera los 30 caracteres
+  if (!regex.test(key) && !teclasEspeciales.includes(key)) { 
 
-    event.preventDefault(); // Evitamos la acción de la tecla
+    event.preventDefault();
   }
+};
+
+export const validarCampo = (event) => {
+
+  let campo = event.target;
+
+  if (((campo.tagName == "INPUT" || campo.tagName == "TEXTAREA") && campo.value.trim() == "") || (campo.tagName == "SELECT" && campo.selectedIndex == 0)) {
+    agregarError(campo.parentElement);
+    return false;
+  }
+
+  if (campo.parentElement.className.includes('error'))
+    quitarError(campo.parentElement);
+
+  return true;
+};
+
+export const agregarError = (campo, mensaje = "El campo es obligatorio.") => {
+  campo.classList.add('error');
+  campo.style.setProperty('--error-message', `"${mensaje}"`);
+};
+
+
+export const quitarError = (campo) => {
+  campo.classList.remove('error');
+};
+
+export const validarMaximo = (event, maximo) => {
+  const key = event.key;
+  if (!teclasEspeciales.includes(key) && event.target.value.length >= maximo) event.preventDefault(); 
+};
+
+
+export const datos = {};
+export const validarCampos = (event) => {
+
+  let valido = true;
+
+  const campos = [...event.target].filter((elemento) => elemento.hasAttribute("required") && (elemento.tagName == "INPUT" || elemento.tagName == "TEXTAREA"));
+
+  campos.forEach((campo) => {
+    if (!validarCampo({ target: campo })) valido = false;
+
+    datos[campo.getAttribute("name")] = campo.value;
+  });
+
+  return valido;
 };
--- src/views/Categorias/nuevaCategoriaController.js
+++ src/views/Categorias/nuevaCategoriaController.js
@@ -2,11 +2,28 @@ import * as validate from "../../helpers/validaciones.js";
 
 
 export function nuevaCategoriaController() {
-  let btnAgregar = document.querySelector('#btnAgregar');
+  let formulario = document.querySelector('#form');
 
   let nombre = document.querySelector('[name="nombre"]');
   let descripcion = document.querySelector('[name="descripcion"]');
 
-  nombre.addEventListener("keydown", (e) => validate.validarTexto(e));
-  descripcion.addEventListener("keydown", (e) => validate.validarTexto(e));
+  nombre.addEventListener("keydown", (e) => {
+    validate.validarTexto(e); 
+    validate.validarMaximo(e, 20);
+  });
+
+  descripcion.addEventListener("keydown", (e) => {
+    validate.validarTexto(e);
+    validate.validarMaximo(e, 100);
+  });
+
+  nombre.addEventListener("blur", validate.validarCampo);
+  descripcion.addEventListener("blur", validate.validarCampo);
+
+  formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(validate.validarCampos(e)) console.log(validate.datos);
+    
+  })
 }
